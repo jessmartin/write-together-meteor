2df997051e93922f314d0f193c8241cfdccba21f
Jess Martin <jessmartin@gmail.com>
2013-09-04 17:14:26 -0400

    Adding document list

--- write-together.js
+++ write-together.js
@@ -1,13 +1,41 @@
+// Documents -- {title: String,
+//               content: String}
+Documents = new Meteor.Collection("documents");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+  Deps.autorun(function () {
+    Meteor.subscribe("alldocuments");
+  });
+
+  Template.document.greeting = function () {
     return "Welcome to write-together.";
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
+  Template.document.events({
+    'click button' : function () {
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+      // Save the document
+    },
+    'keypress #doc-title' : function () {
+      if (typeof console !== 'undefined')
+        console.log("You changed the title");
+      // Write the new title for the document
+    },
+    'keypress #doc-content' : function () {
+      if (typeof console !== 'undefined')
+        console.log("You changed the content");
+      // Write the new content for the document
+    }
+  });
+
+  Template.list.helpers({
+    documents: function () {
+      return [
+        {"title": "My document"},
+        {"title": "Your document"},
+        {"title": "Their document"}
+      ];
     }
   });
 }
@@ -15,5 +43,10 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  // Publish all documents
+  Meteor.publish('alldocuments', function () {
+    return Documents.find({});
   });
 }
